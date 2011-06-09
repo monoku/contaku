01e849047286f84b4fc87dea3f1211e41e5ff7d4
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-06-09 15:53:39 -0500

    Action name in context_processors

--- context_processors.py
+++ context_processors.py
@@ -7,8 +7,11 @@ from contaku.forms import ContactForm, SimpleContactForm
 USE_SIMPLE_CONTACT = getattr(settings, 'USE_SIMPLE_CONTACT', True)
 
 def contact(request):
+    data = {}
     if USE_SIMPLE_CONTACT:
         contact_form = SimpleContactForm()
     else:
         contact_form = ContactForm()
-    return {'contact_form':contact_form}
+    data['contact_form'] = contact_form
+    data['contact_action'] = 'contact-action'
+    return data
