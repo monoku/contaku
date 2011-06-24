04b5dbf26e351d783f5b4bdc6773681a4d76616d
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-06-24 16:13:08 -0500

    Initial view and i18n files

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 
 USE_SIMPLE_CONTACT = getattr(settings, 'USE_SIMPLE_CONTACT', True)
@@ -11,10 +12,14 @@ else:
     from contaku.forms import ContactForm
 
 def contact(request):
+    #TODO: implement strategy pattern
     if request.method == 'POST':
         form = ContactForm(data=request.POST)
         if form.is_valid():
-            c = form.save()            
+            form.save()
+            messages.success(request, _("The message of contact was send succesfull"))
         else:
-            pass
+            for key, value in form.errors.iteritems():
+                label = form.fields[key].label
+                messages.warning(request, '%s: %s' % (label, ''.join([v for v in value])))
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
