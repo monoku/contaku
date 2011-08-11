8b125f1f994d459fb57d88a2208bdcc26ea199d9
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-08-11 16:11:04 -0500

    Fixed url

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from contaku.views import contact, contact_json
 
 urlpatterns = patterns('',
     url(r'^$', contact, name='contact-action'),
-    url(r'^json/$', contact, name='contact-action-json'),
+    url(r'^json/$', contact_json, name='contact-action-json'),
 )
